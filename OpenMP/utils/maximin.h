dc796c7e558ff7c0dd0c61052be4255b8fdf00cc
zer0rbt <fedorsh2004@gmail.com>
2024-12-16 04:05:09 +0300

    feat(OpenMP/task9): create highlyParallelMaxFromMin func

--- OpenMP/utils/maximin.h
+++ OpenMP/utils/maximin.h
@@ -25,4 +25,23 @@ T maxFromMin(std::vector<std::vector<T>> matrix) {
     return max_value;
 }
 
+template<typename T>
+T highlyParallelMaxFromMin(std::vector<std::vector<T>> matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int max_value = std::numeric_limits<int>::min();
+#pragma omp parallel for reduction(max:max_value)
+    for (int i = 0; i < n; i++) {
+        int min_value = std::numeric_limits<int>::max();
+#pragma omp parallel for reduction(min:min_value)
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] < min_value) {
+                min_value = matrix[i][j];
+            }
+        }
+        max_value = std::max(max_value, min_value);
+    }
+    return max_value;
+}
+
 #endif //SPBU_HPC_MAXIMIN_H
