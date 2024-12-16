96d8125bcda5dee45a3b2021cc885f6b6fd46d25
zer0rbt <fedorsh2004@gmail.com>
2024-12-16 03:45:44 +0300

    feat(OpenMP/task3): create DefiniteIntegral.h

---
+++ OpenMP/utils/DefiniteIntegral.h
@@ -0,0 +1,17 @@
+#ifndef SPBU_HPC_DEFINITEINTEGRAL_H
+#define SPBU_HPC_DEFINITEINTEGRAL_H
+
+template<typename func>
+double definiteIntegral(func Func, double a, double b, int N){
+    double h = (b - a) / N;
+    double y = 0;
+
+#pragma omp parallel for reduction(+: y)
+    for (int i = 0; i < N; i++){
+        y += Func(i * h);
+    }
+
+    return y * h;
+}
+
+#endif //SPBU_HPC_DEFINITEINTEGRAL_H
